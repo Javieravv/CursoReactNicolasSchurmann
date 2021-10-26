6191928b77ea6af4ea3e0282d6085f44eae59ae9
Javier Armando Vargas Vega <javieravargasestudiante@gmail.com>
2021-10-25 22:13:23 -0500

    Primera app en React

---
+++ myapp1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Estamos aprendiendo React</h1>
+      <p>
+      No ha sido fácil porque es difícil configurlo en Windows10.
+      </p>
+    </div>
+  );
+}
+
+export default App;
